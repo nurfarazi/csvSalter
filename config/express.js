5fcf4dd348aac7abe2c016f2b923432e9953abf0
nurfarazi <nur369188@gmail.com>
2017-03-26 21:53:23 +0600

    readme fix , and added vody parse

--- config/express.js
+++ config/express.js
@@ -1,7 +1,17 @@
-var express = require('express');
+let express = require('express');
+let bodyParser = require('body-parser');
+
 module.exports = function () {
 	var app = express();
 	app.use(express.static('./public'));
+	
+	app.use(bodyParser.json({
+		limit: '50mb'
+	}));
+	app.use(bodyParser.urlencoded({
+		limit: '50mb',
+		extended: true
+	}));
 	
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
